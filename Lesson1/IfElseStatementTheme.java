0f4f0718bb085bae055d2d0d760787e5a9c3f9db
Viktor <pereedz@gmail.com>
2024-12-08 14:50:06 +0300

    Внёс замечания в задачи 3,4,5,6,7,8

--- Lesson1/IfElseStatementTheme.java
+++ Lesson1/IfElseStatementTheme.java
@@ -44,123 +44,111 @@ public class IfElseStatementTheme {
         }
 
         System.out.println("\n3. Проверка числа");
-        int digit = -90;
-        if (digit > 0) {
-            System.out.printf("%d является положительным%s", digit, 
-                    digit % 2 == 0 ? " и четным" : " и нечетным");
-        } else if (digit < 0) {
-            System.out.printf("%d является отрицательным%s", digit, 
-                    digit % 2 == 0 ? " и четным" : " и нечетным");
+        int originNumber = 32;
+        if (originNumber == 0) {
+            System.out.println(originNumber + " = 0");
         } else {
-            System.out.println(digit + " = 0");
+            if (originNumber < 0) { 
+                System.out.print(originNumber + " является отрицательным и ");
+            } else if (originNumber > 0) {
+                System.out.print(originNumber + " является положительным и ");
+            }
+            if (originNumber % 2 == 0) {
+                System.out.printf("четным");
+            } else {
+                System.out.printf("нечетным");
+            } 
         }
-
+  
         System.out.println("\n\n4. Поиск одинаковых цифр в числах");
-        int firstDigit = 123;
-        int secondDigit = 223;
-        System.out.println("Исходные числа: y = " + firstDigit + ", z = " + secondDigit);
-
-        int oneY = firstDigit % 10;
-        int oneZ = secondDigit % 10;
-        int tenY = firstDigit % 100 / 10;
-        int tenZ = secondDigit % 100 / 10;
-        int thousandY = firstDigit / 100;
-        int thousandZ = secondDigit / 100;
-
-        if (!(oneY == oneZ) && !(tenY == tenZ) && !(thousandY == thousandZ)) {
+        int x = 123;
+        int y = 223;
+        System.out.println("Исходные числа: y = " + x + ", z = " + y);
+        int onesX = x % 10;
+        int onesY = y % 10;
+        int tensX = x % 100 / 10;
+        int tensY = y % 100 / 10;
+        int hundredsX = x / 100;
+        int hundredsY = y / 100;
+        if (!(onesX == onesY) && !(tensX == tensY) && !(hundredsX == hundredsY)) {
             System.out.println("Равных чисел нет");
-            return;
         } else {
-            if (oneY == oneZ) {
-                System.out.println("В 1 разряде равны числа " + oneY);
+            if (onesX == onesY) {
+                System.out.println("В 1 разряде равны числа " + onesX);
             }
-            if (tenY == tenZ) {
-                System.out.println("Во 2 разряде равны числа " + tenY);
+            if (tensX == tensY) {
+                System.out.println("Во 2 разряде равны числа " + tensX);
             }
-            if (thousandY == thousandZ) {
-                System.out.println("В 3 разряде равны числа " + thousandY);
+            if (hundredsX == hundredsY) {
+                System.out.println("В 3 разряде равны числа " + hundredsX);
             }
         }
 
         System.out.println("\n5. Определение символа по его коду");
-        char myChar = '\u0031';
-        if (myChar >= 'A' && myChar <= 'Z') {
-            System.out.printf("%c - большая буква", myChar);
-        } else if (myChar >= 'a' && myChar <= 'z') {
-            System.out.printf("%c - маленькая буква", myChar);
-        } else if (myChar >= '0' && myChar <= '9') {
-            System.out.printf("%c - цифра", myChar);
+        char unknownChar = '\u0031';
+        if (unknownChar >= 'A' && unknownChar <= 'Z') {
+            System.out.printf("%c - большая буква", unknownChar);
+        } else if (unknownChar >= 'a' && unknownChar <= 'z') {
+            System.out.printf("%c - маленькая буква", unknownChar);
+        } else if (unknownChar >= '0' && unknownChar <= '9') {
+            System.out.printf("%c - цифра", unknownChar);
         } else {
-            System.out.print(myChar + " - ни буква, ни цифра");
+            System.out.print(unknownChar + " - ни буква, ни цифра");
         }
 
         System.out.println("\n\n6. Подсчет начисленных банком %");
         BigDecimal deposit = new BigDecimal("321123.59");
-        BigDecimal firstCompareNumber = new BigDecimal("100000");
-        BigDecimal secondCompareNumber = new BigDecimal("300000");
-        int compareWithFirst = deposit.compareTo(firstCompareNumber);
-        int compareWithSecond = deposit.compareTo(secondCompareNumber);
-        BigDecimal percent;
+        int compareWithFirst = deposit.compareTo(BigDecimal.valueOf(100_000));
+        int compareWithSecond = deposit.compareTo(BigDecimal.valueOf(300_000));
+        BigDecimal percent = BigDecimal.valueOf(0.1).multiply(deposit);
         if (compareWithFirst < 0) {
-            percent = new BigDecimal(0.05).multiply(deposit); 
+            percent = BigDecimal.valueOf(0.05).multiply(deposit); 
         } else if (compareWithFirst > 0 && compareWithSecond < 0) {
-            percent = new BigDecimal(0.07).multiply(deposit);
-        } else {
-            percent = new BigDecimal(0.1).multiply(deposit);
-        }
+            percent = BigDecimal.valueOf(0.07).multiply(deposit);
+        } 
         BigDecimal result = deposit.add(percent);
         System.out.println("Сумма вклада = " + deposit + " RUB" + "\n" + 
                 "Сумма начисленного процента = " + percent.setScale(2, RoundingMode.HALF_UP) + " RUB" + 
                 "\n" + "Итоговая сумма = " + result.setScale(2, RoundingMode.HALF_UP) + " RUB");
 
         System.out.println("\n7. Определение оценки по предметам");
-        int markHistory = 0;
+        double historyMark = 5;
         int historyPercent = 59;
         if (historyPercent <= 60) {
-            markHistory = 2; 
-        } 
-        if (historyPercent > 60) {
-            markHistory = 3;
-        } 
-        if (historyPercent > 73) {
-            markHistory = 4;
-        } 
-        if (historyPercent > 91) {
-            markHistory = 5;
+            historyMark = 2; 
+        } else if (historyPercent > 60 && historyPercent <= 73) {
+            historyMark = 3;
+        } else if (historyPercent > 73 && historyPercent <= 91) {
+            historyMark = 4;
         }
-        System.out.println("История: " + markHistory);
+        System.out.println("История: " + historyMark);
         int programmingPercent = 92;
-        int mrakProgramming = 0;
+        double programmingMark = 5;
         if (programmingPercent <= 60) {
-            mrakProgramming = 2;
-        } 
-        if (programmingPercent > 60) {
-            mrakProgramming = 3;
-        } 
-        if (programmingPercent > 73) {
-            mrakProgramming = 4;
-        } 
-        if (programmingPercent > 91) {
-            mrakProgramming = 5;
+            programmingMark = 2;
+        } else if (programmingPercent > 60 && programmingPercent <= 73) {
+            programmingMark = 3;
+        } else if (programmingPercent > 73 && programmingPercent <= 91) {
+            programmingMark = 4;
         }
-        System.out.println("Программирование: " + mrakProgramming);
-        double averageMark = (markHistory + mrakProgramming) / 2;
+        System.out.println("Программирование: " + programmingMark);
+        double averageMark = (historyMark + programmingMark) / 2;
         double averagePercent = (historyPercent + programmingPercent) / 2;
         System.out.println("Средний балл по предметам: " + averageMark);
         System.out.println("Средние проценты по предметам: " + averagePercent);
 
         System.out.println("\n8. Расчет годовой прибыли");
         BigDecimal saleInMonth = new BigDecimal("13025.233");
-        // аренда помещения в месяц
         BigDecimal rentCost = new BigDecimal("5123.018");
         BigDecimal priceCost = new BigDecimal("9001.729");
-        BigDecimal monthNumbers = new BigDecimal("12");
-        BigDecimal annualProfit = ((saleInMonth.multiply(monthNumbers))
-                .subtract(priceCost.multiply(monthNumbers))).subtract(rentCost.multiply(monthNumbers));
+        BigDecimal monthNumbers = BigDecimal.valueOf(12);
+        BigDecimal annualProfit = (((saleInMonth.multiply(monthNumbers))
+                .subtract(priceCost.multiply(monthNumbers)))
+                .subtract(rentCost.multiply(monthNumbers))).setScale(2, RoundingMode.HALF_UP);
         if (annualProfit.compareTo(BigDecimal.ZERO) > 0) {
-            System.out.println("Прибыль за год: " + annualProfit.setScale(2, RoundingMode.HALF_UP) + " руб.");
+            System.out.println("Прибыль за год: +" + annualProfit + " руб.");
         } else if (annualProfit.compareTo(BigDecimal.ZERO) < 0) {
-            System.out.println("Прибыль за год: " + annualProfit.setScale(2, RoundingMode.HALF_UP) + " руб.");
+            System.out.println("Прибыль за год: " + annualProfit + " руб.");
         }
     }
 }
