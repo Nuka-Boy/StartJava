c910988436eb008ec0405b1d0b9ad368e3cd180a
Viktor <pereedz@gmail.com>
2025-01-08 15:03:54 +0300

    Внёс замечания в дз 'Угадай число' изменил GuessNumber и применил GuardClauses

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -5,55 +5,56 @@ public class GuessNumber {
     private Scanner scanner;
     private Player player1;
     private Player player2;
-    int number;
     
     public GuessNumber(Player player1, Player player2) {
-        this.scanner = new Scanner(System.in);
         this.player1 = player1;
         this.player2 = player2;
         randomNumber = (int) (Math.random() * 100) + 1;
     }
 
     public void start() {
-        String result;
+        scanner = new Scanner(System.in);
+        int number;
         Player currentPlayer = player1;
       
         while (true) {
-            number = getInput(currentPlayer.getName());
-            result = checkGuess(number);
-            if (result.equals("Вы угадали!")) {
+            String resultGuess;
+            number = getInputPlayer(currentPlayer.getName());
+            resultGuess = checkGuess(number);
+            if (resultGuess.equals("Вы угадали!")) {
                 System.out.println("Игрок " + currentPlayer.getName() + " победил!");
                 return;
             } 
-            System.out.println(result + " Ход следующего игрока"); 
+            System.out.println(resultGuess + " Ход следующего игрока"); 
             currentPlayer = (currentPlayer == player1) ? player2 : player1;  
         }
     }
 
-    private int getInput(String playerName) {        
+    private int getInputPlayer(String playerName) {           
         while (true) {
             System.out.println("\nИгрок " + playerName + ", введите число от 1 до 100: ");
-            if (scanner.hasNextInt()) {
-                number = scanner.nextInt();
-                if (number >= 1 && number <= 100) {
-                    return number;
-                } else {
-                    System.out.println("Некорректное число. Должно быть от 1 до 100");
-                }
-            } else {
+            if (!scanner.hasNextInt()) {
                 System.out.println("Некорректный ввод. Введите целое число");
                 scanner.next();
+                continue;
             }
+            int number = scanner.nextInt();
+            scanner.nextLine();
+            if (number < 1 || number > 100) {
+                System.out.println("Некорректное число. Должно быть от 1 до 100");
+                continue;
+            }    
+            return number;             
         }
     }
-
+    
     private String checkGuess(int number) {
         if (number > randomNumber) {
             return "Введенное число больше загаданного";
-        } else if (number < randomNumber) {
+        } 
+        if (number < randomNumber) {
             return "Введенное число меньше загаданного";
-        } else {
-            return "Вы угадали!";
         }
+        return "Вы угадали!";
     }  
 }
