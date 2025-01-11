043733bca30af1f98787e12c7b79df5db1b7540c
Nuka-Boy <pereedz@gmail.com>
2025-01-11 16:14:46 +0300

    Delete Lesson2/MethodNameUtil.java

--- Lesson2/MethodNameUtil.java
+++
@@ -1,9 +0,0 @@
-public class MethodNameUtil {
-    public static String getCallingMethodName() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        if (stackTrace.length < 3) {
-            return "Невозможно определить вызывающий метод";
-        }
-        return stackTrace[2].getMethodName();
-    }
-}
