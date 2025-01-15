4733725b92dbd620e2afdc6dfcefda204dc98ecc
Viktor <pereedz@gmail.com>
2025-01-15 23:14:55 +0300

    Внёс замечания выводов в консоль в 'Именование методов'

--- Lesson2/BooleanMethods.java
+++ Lesson2/BooleanMethods.java
@@ -1,52 +1,52 @@
 public class BooleanMethods {
     public boolean isProgramRunning() {
-        System.out.print("\n" + MethodNameUtil.getCurrent() + " -> Программа выполняется ? ");
+        System.out.print("\n" + MethodNameUtil.getCurrent() + "() -> Программа выполняется? ");
         return true;
     }
 
     public boolean isFileDeleted() {
-        System.out.print(MethodNameUtil.getCurrent() + " -> Файл удален ? ");
+        System.out.print(MethodNameUtil.getCurrent() + "() -> Файл удален? ");
         return true;
     }
 
     public boolean hasUniqueDigit() {
-        System.out.print(MethodNameUtil.getCurrent() + " -> Содержит уникальную цифру ? ");
+        System.out.print(MethodNameUtil.getCurrent() + "() -> Содержит уникальную цифру? ");
         return false;
     }
 
     public boolean isInputLetter() {
-        System.out.print(MethodNameUtil.getCurrent() + " -> Пользователь ввел букву ? ");
+        System.out.print(MethodNameUtil.getCurrent() + "() -> Пользователь ввел букву? ");
         return true;
     }
 
     public boolean hasEqualDigits() {
-        System.out.print(MethodNameUtil.getCurrent() + " -> Есть ли в числах равные цифры ? ");
+        System.out.print(MethodNameUtil.getCurrent() + "() -> Есть ли в числах равные цифры? ");
         return true;
     }
 
     public boolean hasAttemptsLeft() {
-        System.out.print(MethodNameUtil.getCurrent() + " -> Попытки остались ? ");
+        System.out.print(MethodNameUtil.getCurrent() + "() -> Попытки остались? ");
         return false;
     }
 
     public boolean isEmptyString() {
-        System.out.print(MethodNameUtil.getCurrent() + " -> Пустая строка ? ");
+        System.out.print(MethodNameUtil.getCurrent() + "() -> Пустая строка? ");
         return true;
     }
 
     public boolean isDiceEven() {
-        System.out.print(MethodNameUtil.getCurrent() + " -> Число четное ? ");
+        System.out.print(MethodNameUtil.getCurrent() + "() -> Число четное? ");
         return true;
     }
 
     public boolean isPathValid() {
-        System.out.print(MethodNameUtil.getCurrent() + " -> Путь до файла действителен ? ");
+        System.out.print(MethodNameUtil.getCurrent() + "() -> Путь до файла действителен? ");
         return true;
     }
 
     public boolean isFileExists() {
         System.out.print(MethodNameUtil.getCurrent() + 
-                " -> Существует файл по указанному адресу ? ");
+                "() -> Существует файл по указанному адресу? ");
         return true;
     }
 }
--- Lesson2/MethodsTest.java
+++ Lesson2/MethodsTest.java
@@ -1,6 +1,28 @@
 public class MethodsTest {
     public static void main(String[] args) {
+        MethodsTest methodsTest = new MethodsTest();
         System.out.println("\nВыполнение методов из класса NonBooleanMethods:");
+        methodsTest.nonBooleanMethods();
+        System.out.println();
+        System.out.println("\nВыполнение методов из класса BooleanMethods:");
+        methodsTest.booleanMethods();
+    }
+
+    public void booleanMethods() {
+        BooleanMethods bm = new BooleanMethods();
+        System.out.println(bm.isProgramRunning());
+        System.out.println(bm.isFileDeleted()); 
+        System.out.println(bm.hasUniqueDigit()); 
+        System.out.println(bm.isInputLetter()); 
+        System.out.println(bm.hasEqualDigits()); 
+        System.out.println(bm.hasAttemptsLeft()); 
+        System.out.println(bm.isEmptyString()); 
+        System.out.println(bm.isDiceEven()); 
+        System.out.println(bm.isPathValid()); 
+        System.out.println(bm.isFileExists()); 
+    }
+
+    public void nonBooleanMethods() {
         NonBooleanMethods nonBm = new NonBooleanMethods();
         nonBm.findLongestWord();
         nonBm.selectMenuItem();
@@ -16,20 +38,5 @@ public class MethodsTest {
         nonBm.inputMathExpression();
         nonBm.findWinner();
         nonBm.findBookByAuthor();
-
-        System.out.println();
-
-        System.out.println("\nВыполнение методов из класса BooleanMethods:");
-        BooleanMethods bm = new BooleanMethods();
-        System.out.println(bm.isProgramRunning());
-        System.out.println(bm.isFileDeleted()); 
-        System.out.println(bm.hasUniqueDigit()); 
-        System.out.println(bm.isInputLetter()); 
-        System.out.println(bm.hasEqualDigits()); 
-        System.out.println(bm.hasAttemptsLeft()); 
-        System.out.println(bm.isEmptyString()); 
-        System.out.println(bm.isDiceEven()); 
-        System.out.println(bm.isPathValid()); 
-        System.out.println(bm.isFileExists()); 
     }
 }
