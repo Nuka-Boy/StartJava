cbd53aeefb9c3c0d9fae23702475ce720f179fa0
Viktor <pereedz@gmail.com>
2025-01-26 14:09:23 +0300

    изменил папку

--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -1,7 +1,7 @@
 package com.startjava.lesson_2_3_4.guess;
 
 public class Player {
-    private String name;
+    private final String name;
 
     public Player(String name) {
         this.name = name;
